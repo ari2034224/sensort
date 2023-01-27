d6f173eb17c408af25f5c1f8cbc51d924b61ddef
Ariadna <2034224@upt.edu.mx>
2023-01-26 23:57:37 -0600

    cambios

--- Tarea_SensorProximidad/app/src/main/java/com/example/tarea_sensorproximidad/MainActivity.java
+++ Tarea_SensorProximidad/app/src/main/java/com/example/tarea_sensorproximidad/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.tarea_sensorproximidad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    RelativeLayout main;
+    TextView text;
+    SensorManager senma;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
